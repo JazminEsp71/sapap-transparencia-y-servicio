5eb491dd46ff9c90c7f016239fb70e07e298ab21
JazminEsp71 <jazminesp71@gmail.com>
2026-05-04 22:06:55 -0600

    direccion actualizada

--- src/lib/contacto.api.ts
+++ src/lib/contacto.api.ts
@@ -1,4 +1,4 @@
-const API_URL = "http://192.168.10.145:3000/contacto";
+const API_URL = "http://192.168.10.145:3000";
 //const API_URL = "http://localhost:3000";
 
 export const enviarContacto = async (data: any) => {
